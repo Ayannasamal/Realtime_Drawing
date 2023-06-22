9fcd563e9126b5ff8e8f74140ff5b1b4f477045f
Ayannasamal <96683706+Ayannasamal@users.noreply.github.com>
2023-06-22 18:52:26 +0530

    Add files via upload

--- main1.js
+++ main1.js
@@ -1,3 +1,10 @@
+nosex = 0;
+nosey = 0;
+leftwristx = 0;
+rightwristx = 0;
+difference = 0;
+
+
 function setup(){
     video=createCapture (VIDEO);
     video.size(550, 500);
@@ -11,6 +18,10 @@ function setup(){
 
 function draw(){
     background("#d3d3d3");
+    fill("#F90093");
+    stroke("#F90093");
+    square(nosex, nosey, difference);
+    document.getElementById("square_side").innerHTML="The Width and Height of the Square is " +difference+ "px";
 }
 
 function modelLoaded(){
@@ -20,6 +31,15 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        nosex = results[0].pose.nose.x;
+        nosey = results[0].pose.nose.y;
+        console.log("nosex = " +nosex+ " nosey = " +nosey);
+
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        difference = floor(leftwristx - rightwristx);
+        console.log("leftwristx = " +leftwristx+ " rightwristx = " +rightwristx+ " difference = " +difference);
+        
     }
 }
 
